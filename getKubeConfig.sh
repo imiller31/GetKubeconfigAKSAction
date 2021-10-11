e98cf36cdb83ba0c09b65e45ffe751bf4f311cad
Israel Miller <ismille@microsoft.com>
2021-10-11 16:11:31 -0400

    adding repository to gh secret cmd

--- getKubeConfig.sh
+++ getKubeConfig.sh
@@ -4,4 +4,4 @@ az login --service-principal -u $APP_ID -p $APP_SECRET --tenant $TENANT_ID
 
 az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP_NAME --subscription $SUBSCRIPTION_ID
 
-gh secret set $SECRET_NAME < ~/.kube/config
+gh secret set $SECRET_NAME -r $REPOSITORY < ~/.kube/config
